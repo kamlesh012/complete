d6900d37d5e3f86ecc89e47614d9e9859cbc7492
Kamlesh Singh Bisht <75537446+kamlesh012@users.noreply.github.com>
2023-09-10 12:12:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,10 @@ const port = process.env.PORT || 5000;
 connectDB();
 
 const app = express();
+import cors from 'cors';
 
 app.use(express.json());
+app.use(cors({origin: '*'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
@@ -29,23 +31,11 @@ app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
 
-// if (process.env.NODE_ENV === 'production') {
-//   const __dirname = path.resolve();
-//   app.use('/uploads', express.static('/var/data/uploads'));
-//   app.use(express.static(path.join(__dirname, '/frontend/build')));
-
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//   );
-// } else
-{
   const __dirname = path.resolve();
   app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
     res.send('API is running....');
   });
-}
-
 app.use(notFound);
 app.use(errorHandler);
 
